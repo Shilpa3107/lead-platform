217f5d20c294134c91d0c07c3a2762318dbc7ee3
Shilpa3107 <shilpa.sinha3107@gmail.com>
2026-07-26 22:45:42 +0530

    Added correct frontend deployment link

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "lead-platform-iota.vercel.app",  
+        "https://lead-platform-iota.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
